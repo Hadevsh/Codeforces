994fe8183a2f9717eed721721c900c36c3a1085c
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2025-07-09 12:16:16 +0200

    Add problem 282A in Python

---
+++ A's/py/282A.py
@@ -0,0 +1,16 @@
+def main() -> None:
+    t: int = int(input())
+    res: int = 0
+
+    while t:
+        w: str = str(input())
+
+        if w[0] == "-" or w[len(w) - 1] == "-": res -= 1
+        else: res += 1
+
+        t -= 1
+    
+    print(res)
+
+if __name__ == "__main__":
+    main()
