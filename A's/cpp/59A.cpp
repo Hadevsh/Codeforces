99a5aeef4d04f6c69022c34edb3b5643c565916a
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2025-07-12 15:27:04 +0200

    Add problem 59A in C++

---
+++ A's/cpp/59A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s; cin >> s;
+
+    string uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    string lowercase = "qwertyuiopasdfghjklzxcvbnm";
+
+    int n = s.size();
+    int u {0}; int l {0};
+    for (int i = 0; i < n; i++) {
+        if (uppercase.find(s[i]) != string::npos) {  // If in uppercase
+            u++;
+        } else {
+            l++;
+        }
+    }
+
+    if (u > l) {  // More uppercase letters
+        transform(s.begin(), s.end(), s.begin(), ::toupper);
+    } else {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+    }
+
+    cout << s << endl;
+    
+    return 0;
+}
