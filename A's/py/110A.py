34e2a8e934a55eb821cff1ed0f52a4974160b3ac
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2025-07-13 19:02:00 +0200

    Add problem fix 110A in Python

--- A's/py/110A.py
+++ A's/py/110A.py
@@ -5,7 +5,7 @@ def main() -> None:
     for c in n:
         if c == "4" or c == "7": l += 1
 
-    print("YES" if l == "4" or l == "7" else "NO")
+    print("YES" if (l == 4 or l == 7) else "NO")
 
 if __name__ == "__main__":
     main()
