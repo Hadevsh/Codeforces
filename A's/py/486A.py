d256dee2f87000b6bc6d538c75e7271f82839038
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2025-07-21 21:45:32 +0200

    Add problem 486A in Python

---
+++ A's/py/486A.py
@@ -0,0 +1,11 @@
+def main() -> None:
+    n: int = 0
+    if n % 2 == 0:
+        # n = 2k -> f(n) = k
+        print(n / 2)
+    else:
+        # n = 2k + 1 -> f(n) = -(k + 1) = -((n + 1) / 2)
+        print(-((n + 1) / 2))
+
+if __name__ == "__main__":
+    main()
