3d2065528b1c35f18c830230be9440f4f1669c73
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2025-07-28 11:40:58 +0200

    Add problem 2126A in C++

---
+++ A's/cpp/2126A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--)
+    {
+        int x; cin >> x;
+        int y = 0;
+        string x_s = to_string(x);
+        string y_s = to_string(y);
+        bool found = false;
+        while (!found) {
+            for (char c : y_s) {
+                if (x_s.find(c) != string::npos) {
+                    cout << y << endl;
+                    found = true;
+                }
+            }
+            y++;
+            y_s = to_string(y);
+        }
+    }
+    
+    
+    return 0;
+}
