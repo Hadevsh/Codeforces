99b88a5a67eace17de28cfab79129579470a0e11
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2025-07-28 12:11:56 +0200

    Add problem 510A in C++

---
+++ A's/cpp/510A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, m; cin >> n >> m;
+    bool right = true;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (i % 2 == 0) cout << "#";
+            else {
+                if (!right && j == 0) cout << "#";
+                else if (right && j == m - 1) cout << "#";
+                else cout << ".";
+                right = !right;
+            }
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
