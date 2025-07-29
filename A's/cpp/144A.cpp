086c8c303790b39d3f112fc78dafdf2a706c7d95
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2025-07-29 15:31:31 +0200

    Fix problem 144A in C++

--- A's/cpp/144A.cpp
+++ A's/cpp/144A.cpp
@@ -9,24 +9,33 @@ int main() {
         cin >> s[i];
     }
 
-    int mi, ma = s[0];
-    int mi_i, ma_i = 0;
+    int mi = s[0], ma = s[0];
+    int mi_i = 0, ma_i = 0;
     for (int i = 0; i < n; i++) {
         mi = min(s[i], mi);
         ma = max(s[i], ma);
     }
+
+    // Find the leftmost occurrence of the maximum value
     for (int i = 0; i < n; i++) {
-        if (s[i] == ma) ma_i = i;
-    }
-    for (int i = n - 1; i > 0; i--) {
-        if (s[i] == mi) mi_i = i;
+        if (s[i] == ma) {
+            ma_i = i;
+            break; // Leftmost occurrence
+        }
     }
 
-    int secs = 0;
-    if (mi_i < ma_i) {  // If min height is before the max height
-        secs = ma_i + (n - mi_i) - 2;
-    } else {
-        secs = ma_i + (n - mi_i);
+    // Find the rightmost occurrence of the minimum value
+    for (int i = n - 1; i >= 0; i--) {
+        if (s[i] == mi) {
+            mi_i = i;
+            break; // Rightmost occurrence
+        }
+    }
+
+    // Calculate the total seconds
+    int secs = ma_i + (n - 1 - mi_i);
+    if (mi_i < ma_i) {
+        secs--; // Subtract 1 if they cross each other
     }
     cout << secs << endl;
     
